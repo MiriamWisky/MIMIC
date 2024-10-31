1bae0a2c24afadb4fefe7b93888ba87b9514078d
Ephi (efi) Cohen <cbf252@gmail.com>
2024-10-31 18:09:19 +0000

    "Update entry_point.sh to utilize Docker exec for running PostgreSQL commands, correct file paths, and adjust command formatting. Although the command runs, there are some issues when executing the SQL scripts."

--- scripts/entry_point.sh
+++ scripts/entry_point.sh
@@ -1,4 +1,4 @@
-psql -d postgres -f ./create.sql
-psql -d postgres -v ON_ERROR_STOP=1 -v mimic_data_dir=mimic-iv2 -f ./load_gz.sql
-psql -d postgres -v ON_ERROR_STOP=1 -v mimic_data_dir=mimic-iv2 -f ./constraint.sql
-psql -d postgres -v ON_ERROR_STOP=1 -v mimic_data_dir=mimic-iv2 -f ./index.sql
+docker exec -it postgres_container psql -U postgres -f /data/scripts/create.sql && \
+docker exec -it postgres_container psql -U postgres -v ON_ERROR_STOP=1 -v mimic_data_dir=/data/mimic-iv2 -f /data/scripts/load_gz.sql && \
+docker exec -it postgres_container psql -U postgres -v ON_ERROR_STOP=1 -v mimic_data_dir=/data/mimic-iv2 -f /data/scripts/constraint.sql && \
+docker exec -it postgres_container psql -U postgres -v ON_ERROR_STOP=1 -v mimic_data_dir=/data/mimic-iv2 -f /data/scripts/index.sql
